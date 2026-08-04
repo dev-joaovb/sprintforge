4294983698d2e0d268433d4cfb6d9641266f0420
João Victor Bueno Carneiro <114323727+dev-joaovb@users.noreply.github.com>
2026-08-04 12:33:19 -0300

    Sprint 9, SCRUM: Product Backlog

--- frontend/src/features/scrum/components/ProductBacklog.jsx
+++ frontend/src/features/scrum/components/ProductBacklog.jsx
@@ -1,24 +1,72 @@
 import { useScrum } from "../../../context/ScrumContext";
+import { useState } from "react";
+
+import StoryModal from "./StoryModal";
 import UserStoryCard from "./UserStoryCard";
 
 const ProductBacklog = () => {
-    const { stories } = useScrum();
-    return (
-        <div
-        className="
-            flex
-            flex-col
-            gap-4
+  const { stories } = useScrum();
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const [selectedStory, setSelectedStory] = useState(null);
+
+  const handleNewStory = () => {
+    setSelectedStory(null);
+    setModalOpen(true);
+  };
+
+  const handleEditStory = (story) => {
+    setSelectedStory(story);
+    setModalOpen(true);
+  };
+
+  return (
+    <>
+      <div className="flex justify-end mb-4">
+        <button
+          onClick={handleNewStory}
+          className="
+          bg-blue-600
+          hover:bg-blue-700
+          transition
+          px-4
+          py-2
+          rounded-lg
+          font-medium
         "
         >
+          + Nova User Story
+        </button>
+      </div>
+
+      <div
+        className="
+        flex
+        flex-col
+        gap-4
+      "
+      >
         {stories.map((story) => (
-            <UserStoryCard
+          <UserStoryCard
             key={story.id}
             story={story}
-            />
+            onClick={handleEditStory}
+          />
         ))}
-        </div>
-    );
+      </div>
+
+      <StoryModal
+        open={modalOpen}
+        story={selectedStory}
+        onClose={() => {
+          setSelectedStory(null);
+          setModalOpen(false);
+        }}
+        onSave={() => {}}
+        onDelete={() => {}}
+      />
+    </>
+  );
 };
 
 export default ProductBacklog;
