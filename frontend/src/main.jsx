d7f4ada630dac065af7763430c259b73835e5d75
João Victor Bueno Carneiro <114323727+dev-joaovb@users.noreply.github.com>
2026-08-19 21:25:10 -0300

    Sprint 9 SCRUM Stories

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,5 @@
+// main principal para renderizar a aplicação React, incluindo os provedores de contexto para Agile, Kanban, Scrum e Toast. Ele importa os estilos globais e o componente principal App, garantindo que todos os componentes da aplicação tenham acesso aos estados compartilhados através dos contextos.
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
