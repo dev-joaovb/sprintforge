e38e19ff4aca0ad87619151d65ee9bc9866046b0
João Victor Bueno Carneiro <114323727+dev-joaovb@users.noreply.github.com>
2026-09-02 10:22:12 -0300

    Novas atualizações com o backend

--- SprintForge/server.ts
+++ SprintForge/server.ts
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 
 // Security & Parsing Middleware
 app.use(
